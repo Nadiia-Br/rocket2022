8171906f01a40b735984f97e6e1ec0bfd5dd8fa2
Nadiia-Br <nadiia.brn@gmail.com>
2022-10-24 00:37:00 +0300

    lesson1

---
+++ basic/lesson1/lesson1.js
@@ -0,0 +1,20 @@
+let name = 'Nadiia';
+let age = 22;
+let hasCar = false;
+
+
+let user = {
+    name: 'Nadiia',
+    age: 20,
+    job: {
+        title: 'waitress',
+        payment: '100 hrn'
+    }
+}
+
+let nul = null;
+let und = undefined;
+
+let bInt = BigInt(9937239292);
+
+let symn = Symbol('s');
