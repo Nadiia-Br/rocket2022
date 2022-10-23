d6874847ca8e033f2357889cce4a7147c0556d61
Nadiia-Br <nadiia.brn@gmail.com>
2022-10-24 01:00:39 +0300

    lesson1

--- basic/lesson1/lesson1.js
+++ basic/lesson1/lesson1.js
@@ -2,7 +2,6 @@ let name = 'Nadiia';
 let age = 22;
 let hasCar = false;
 
-
 let user = {
     name: 'Nadiia',
     age: 20,
@@ -18,3 +17,20 @@ let und = undefined;
 let bInt = BigInt(9937239292);
 
 let symn = Symbol('s');
+
+// console.log(user, 'user info');
+// console.log(name, age);
+//
+//
+// console.log(2 + 2 -  2 * 3);
+// console.log(14 % 3);
+//
+//
+// console.log(user.job);
+
+
+console.log(user);
+user.age = 25;
+user.phone = 'Samsung';
+
+console.log(user);
