19961b27592cd9ef424be40ac9360f51554354f9
Nadiia-Br <nadiia.brn@gmail.com>
2022-10-31 23:22:58 +0200

    lesson2

--- basic/lesson1/lesson1.js
+++ basic/lesson1/lesson1.js
@@ -1,5 +1,7 @@
 let name = 'Nadiia';
-let age = 22;
+let age = 14;
+let login = 'Admin';
+
 let hasCar = false;
 
 let user = {
@@ -14,9 +16,9 @@ let user = {
 let nul = null;
 let und = undefined;
 
-let bInt = BigInt(9937239292);
-
-let symn = Symbol('s');
+// let bInt = BigInt(9937239292);
+//
+// let symn = Symbol('s');
 
 // console.log(user, 'user info');
 // console.log(name, age);
@@ -28,9 +30,53 @@ let symn = Symbol('s');
 //
 // console.log(user.job);
 
+//
+// console.log(user);
+// user.age = 25;
+// user.phone = 'Samsung';
+//
+// console.log(user);
 
-console.log(user);
-user.age = 25;
-user.phone = 'Samsung';
 
-console.log(user);
+// if (age > 18){
+//     console.log('WELCOME');
+// }  else if (login === 'Admin'){
+//     console.log('WELCOME');
+// }
+//
+// else{
+//     let message = 'You are too young';
+//     console.log(message);
+// }
+
+
+// || - OR
+//  & - AND
+
+// let isNameAllowed = name === 'Oleg' || name === 'Anna';
+//
+// if (age>18 || login === 'Admin'  &&  isNameAllowed){
+//     console.log('WELCOME');
+// }   else{
+//      let message = 'You are too young';
+//      console.log(message);
+//  }
+
+
+switch (name) {
+    case 'Nadiia':
+        console.log('This is me');
+        break;
+
+    case 'Bird':
+        console.log('SHO TY');
+        break;
+
+    case 'Dimon':
+    case 'Sasha':
+        console.log('Valy ih');
+        break;
+
+    default:
+        console.log('unknown name')
+}
---
+++ basic/lesson2/lesson2.js
@@ -0,0 +1,13 @@
+  const age = 18;
+
+// if (age >= 18){
+//     console.log('YES')
+// } else {
+//     console.log('NO')
+// }
+
+
+  (age > 18  || name === 'admin') ? console.log('YES') : console.log ('NO');
+
+  const isOld = age > 18 ? 'YES' : 'No';
+  console.log(isOld);
